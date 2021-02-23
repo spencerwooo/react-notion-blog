cc2b8569e89c1bcb77b1e76862d076334fd073b6
spencerwooo <spencer.woo@outlook.com>
2021-02-23 21:26:34 +0800

    add pagination

--- pages/[year]/[month]/[slug].tsx
+++ pages/[year]/[month]/[slug].tsx
@@ -1,4 +1,5 @@
-import { CalendarOutline, HomeOutline } from 'heroicons-react'
+import { ArrowLeftOutline, CalendarOutline, ChevronLeftOutline, ChevronRightOutline } from 'heroicons-react'
+import Head from 'next/head'
 import Link from 'next/link'
 import 'prismjs/themes/prism-tomorrow.css'
 import { FC } from 'react'
@@ -8,63 +9,106 @@ import { getAllPosts, Post } from '../..'
 import Footer from '../../../components/Footer'
 import { formatSlug } from '../../../utils/slugFormat'
 
+export interface Pagination {
+  prev: Post | null
+  next: Post | null
+}
+
 export const getStaticProps = async ({ params: { slug } }: { params: { slug: string } }) => {
   // Get all posts again
   const posts = await getAllPosts()
 
   // Find the current blogpost by slug
-  const post = posts.find(t => t.slug === slug)
+  const postIndex = posts.findIndex(t => t.slug === slug)
+  const post = posts[postIndex]
+
+  const pagination: Pagination = {
+    prev: postIndex - 1 >= 0 ? posts[postIndex - 1] : null,
+    next: postIndex + 1 < posts.length ? posts[postIndex + 1] : null
+  }
 
   const blocks = await fetch(`https://notion-api.splitbee.io/v1/page/${post!.id}`).then(res => res.json())
 
   return {
     props: {
       blocks,
-      post
+      post,
+      pagination
     },
     revalidate: 1
   }
 }
 
-const BlogPost: FC<{ post: Post; blocks: BlockMapType }> = ({ post, blocks }: { post: Post; blocks: BlockMapType }) => {
+const BlogPost: FC<{ post: Post; blocks: BlockMapType; pagination: Pagination }> = ({
+  post,
+  blocks,
+  pagination
+}: {
+  post: Post
+  blocks: BlockMapType
+  pagination: Pagination
+}) => {
   if (!post) return null
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="container mx-auto px-6 justify-center flex-grow max-w-4xl">
-        <nav className="mt-4 inline-block">
-          <Link href="/">
-            <a className="flex -ml-2 p-2 rounded lg:hover:bg-gray-100">
-              <HomeOutline className="mr-4" />
-              <span>Home</span>
-            </a>
-          </Link>
-        </nav>
+    <>
+      <Head>
+        <title>{post.name} - Spencer&apos;s Blog</title>
+      </Head>
+      <div className="min-h-screen flex flex-col">
+        <div className="container mx-auto px-6 justify-center flex-grow max-w-4xl">
+          <nav className="mt-4 inline-block">
+            <Link href="/">
+              <a className="flex items-center -ml-2 p-2 rounded lg:hover:bg-gray-100">
+                <ArrowLeftOutline size={20} className="mr-4" />
+                <span>Home</span>
+              </a>
+            </Link>
+          </nav>
 
-        <div className="my-16 mx-auto max-w-3xl">
-          <div className="mb-12 text-center">
-            <div className="text-3xl font-bold mb-3">{post.name}</div>
-            <div className="text-sm text-gray-600 flex items-center justify-center space-x-1">
-              <div className="flex items-center">
-                <CalendarOutline size={16} className="mr-2" />
-                <span>{new Date(post.date).toLocaleDateString()} · </span>
-              </div>
-              <div className="flex items-center space-x-2">
+          <div className="my-16 mx-auto max-w-3xl">
+            <div className="mb-12 text-center">
+              <div className="text-3xl font-bold mb-3">{post.name}</div>
+              <div className="text-sm text-gray-400 flex flex-nowrap justify-center items-center space-x-2 overflow-hidden">
+                <CalendarOutline size={16} className="flex-shrink-0" />
+                <span className="flex-shrink-0">{new Date(post.date).toLocaleDateString()} · </span>
                 {post.author.map(author => (
-                  <div key={author.id} className="flex items-center space-x-1">
+                  <div key={author.id} className="flex items-center space-x-1 flex-shrink-0">
                     <img src={author.profilePhoto} alt="profile photo" className="w-6 h-6 rounded-full" />
-                    <span>{author.fullName}</span>
+                    <span className="hidden md:block">{author.fullName}</span>
                   </div>
                 ))}
               </div>
             </div>
-          </div>
-          <NotionRenderer blockMap={blocks} />
-        </div>
-      </div>
 
-      <Footer />
-    </div>
+            <div className="overflow-hidden">
+              <NotionRenderer blockMap={blocks} />
+            </div>
+
+            <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-4 text-sm text-gray-600">
+              {pagination.prev && (
+                <Link href="/[year]/[month]/[slug]" as={formatSlug(pagination.prev.date, pagination.prev.slug)}>
+                  <a className="p-3 border-2 hover:bg-gray-50 flex items-center justify-between space-x-2">
+                    <ChevronLeftOutline size={20} />
+                    <span>{pagination.prev?.name}</span>
+                  </a>
+                </Link>
+              )}
+              {pagination.next && (
+                <Link href="/[year]/[month]/[slug]" as={formatSlug(pagination.next.date, pagination.next.slug)}>
+                  <a className="p-3 border-2 hover:bg-gray-50 flex items-center justify-between space-x-2">
+                    <span>{pagination.next?.name}</span>
+                    <ChevronRightOutline size={20} />
+                  </a>
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link'
-import { formatSlug } from '../utils/slugFormat'
+import { CalendarOutline, HomeOutline, RssOutline, TagOutline, UserCircleOutline } from 'heroicons-react'
+import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '../components/Footer'
-import { CalendarOutline, HashtagOutline } from 'heroicons-react'
+import { formatSlug } from '../utils/slugFormat'
 
 const NOTION_BLOG_ID = process.env.NOTION_BLOG_ID || '7021cba3b8a04865850473d4037762ad'
 
@@ -41,66 +42,87 @@ export const getStaticProps = async () => {
 
 const HomePage = ({ posts }: { posts: Post[] }) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="container mx-auto px-6 justify-center flex-grow max-w-4xl">
-        <div className="my-16">
-          <div className="inline-block shadow-lg rounded-full w-18 h-18">
-            <Image className="rounded-full" src="/images/avatar.png" alt="avatar" width="100%" height="100%" />
-          </div>
-          <div className="mt-8 text-2xl font-bold flex text-center">Spencer&apos;s Blog</div>
-          <div className="mt-2">
-            <a
-              href="https://spencerwoo.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-1 bg-yellow-50 hover:text-yellow-600"
-            >
-              Portfolio
-            </a>
-            {' / '}
-            <a
-              href="https://spencerwoo.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-1 bg-purple-50 hover:text-purple-600"
-            >
-              Friends
-            </a>
-          </div>
+    <>
+      <Head>
+        <title>Spencer&apos;s Blog</title>
+      </Head>
+      <div className="min-h-screen flex flex-col">
+        <div className="container mx-auto px-6 justify-center flex-grow max-w-3xl">
+          <div className="my-16">
+            <div className="inline-block shadow-lg rounded-full w-18 h-18">
+              <Image className="rounded-full" src="/images/avatar.png" alt="avatar" width="100%" height="100%" />
+            </div>
+            <div className="mt-8 text-2xl font-bold">Spencer&apos;s Blog</div>
+            <div className="mt-2 flex items-center space-x-2">
+              <a
+                href="https://spencerwoo.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-1 px-2 bg-yellow-50 hover:text-yellow-600 rounded"
+              >
+                <div className="flex items-center space-x-2">
+                  <HomeOutline size={16} />
+                  <span>Portfolio</span>
+                </div>
+              </a>
+              <a
+                href="https://spencerwoo.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-1 px-2 bg-purple-50 hover:text-purple-600 rounded"
+              >
+                <div className="flex items-center space-x-2">
+                  <UserCircleOutline size={16} />
+                  <span>Friends</span>
+                </div>
+              </a>
+              <a
+                href="https://spencerwoo.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="py-1 px-2 bg-indigo-50 hover:text-indigo-600 rounded"
+              >
+                <div className="flex items-center space-x-2">
+                  <RssOutline size={16} />
+                  <span>RSS</span>
+                </div>
+              </a>
+            </div>
 
-          <div className="mt-12 leading-loose flex flex-col space-y-4 -mx-4">
-            {posts.map(
-              post =>
-                post.published && (
-                  <Link key={post.id} href="/[year]/[month]/[slug]" as={formatSlug(post.date, post.slug)}>
-                    <a className="p-4 hover:bg-gray-50">
-                      <div className="rounded-xl mb-2 px-2 py-1 text-blue-800 bg-blue-100 text-sm inline-block">
-                        <div className="flex items-center space-x-1">
-                          <HashtagOutline size={16} /> <span>{post.tag}</span>
-                        </div>
-                      </div>
-                      <div className="font-bold text-xl mb-1">{post.name}</div>
-                      <div className="text-sm text-gray-400 mb-2">{post.preview}</div>
-                      <div className="text-sm text-gray-400 flex items-center space-x-2">
-                        <CalendarOutline size={16} />
-                        <span>{new Date(post.date).toLocaleDateString()}</span>
-                        {post.author.map(author => (
-                          <div key={author.id} className="flex items-center space-x-1">
-                            <img src={author.profilePhoto} alt="profile photo" className="w-6 h-6 rounded-full" />
-                            <span>{author.fullName}</span>
+            <div className="mt-12 leading-loose flex flex-col space-y-4 -mx-4">
+              {posts.map(
+                post =>
+                  post.published && (
+                    <Link key={post.id} href="/[year]/[month]/[slug]" as={formatSlug(post.date, post.slug)}>
+                      <a className="p-4 hover:bg-gray-50">
+                        <div className="rounded-xl mb-2 px-2 py-1 text-blue-800 bg-blue-100 text-sm inline-block">
+                          <div className="flex items-center space-x-1">
+                            <TagOutline size={16} /> <span>{post.tag}</span>
                           </div>
-                        ))}
-                      </div>
-                    </a>
-                  </Link>
-                )
-            )}
+                        </div>
+                        <div className="font-bold text-xl mb-1">{post.name}</div>
+                        <div className="text-sm text-gray-400 mb-2">{post.preview}</div>
+                        <div className="text-sm text-gray-400 flex flex-nowrap items-center space-x-2 overflow-hidden">
+                          <CalendarOutline size={16} className="flex-shrink-0" />
+                          <span className="flex-shrink-0">{new Date(post.date).toLocaleDateString()}</span>
+                          {post.author.map(author => (
+                            <div key={author.id} className="flex items-center space-x-1 flex-shrink-0">
+                              <img src={author.profilePhoto} alt="profile photo" className="w-6 h-6 rounded-full" />
+                              <span className="hidden md:block">{author.fullName}</span>
+                            </div>
+                          ))}
+                        </div>
+                      </a>
+                    </Link>
+                  )
+              )}
+            </div>
           </div>
         </div>
-      </div>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   )
 }
 
