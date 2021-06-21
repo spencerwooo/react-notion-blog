57b06d35d59079ab4f6d7660e63f1974d2c3d4e3
spencerwooo <spencer.woo@outlook.com>
2021-06-21 15:05:31 +0100

    remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
